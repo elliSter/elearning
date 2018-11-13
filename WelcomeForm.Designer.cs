b643c62f51f83fe86e3f135953e8f672c59e49d3
elliSter <36847336+elliSter@users.noreply.github.com>
2018-11-13 17:15:44 +0100

    Add files via upload

---
+++ WelcomeForm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class WelcomeForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usernameTxtBox = new System.Windows.Forms.TextBox();
+            this.pswrdTxtBox = new System.Windows.Forms.TextBox();
+            this.logInButton = new System.Windows.Forms.Button();
+            this.signUpBbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(149, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hello world!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(89, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "username:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "password:";
+            // 
+            // usernameTxtBox
+            // 
+            this.usernameTxtBox.Location = new System.Drawing.Point(152, 123);
+            this.usernameTxtBox.Name = "usernameTxtBox";
+            this.usernameTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTxtBox.TabIndex = 3;
+            // 
+            // pswrdTxtBox
+            // 
+            this.pswrdTxtBox.Location = new System.Drawing.Point(152, 150);
+            this.pswrdTxtBox.Name = "pswrdTxtBox";
+            this.pswrdTxtBox.Size = new System.Drawing.Size(100, 20);
+            this.pswrdTxtBox.TabIndex = 4;
+            // 
+            // logInButton
+            // 
+            this.logInButton.Location = new System.Drawing.Point(94, 198);
+            this.logInButton.Name = "logInButton";
+            this.logInButton.Size = new System.Drawing.Size(75, 23);
+            this.logInButton.TabIndex = 5;
+            this.logInButton.Text = "Log in";
+            this.logInButton.UseVisualStyleBackColor = true;
+            this.logInButton.Click += new System.EventHandler(this.logInButton_Click);
+            // 
+            // signUpBbutton
+            // 
+            this.signUpBbutton.Location = new System.Drawing.Point(175, 198);
+            this.signUpBbutton.Name = "signUpBbutton";
+            this.signUpBbutton.Size = new System.Drawing.Size(75, 23);
+            this.signUpBbutton.TabIndex = 6;
+            this.signUpBbutton.Text = "Sign up";
+            this.signUpBbutton.UseVisualStyleBackColor = true;
+            this.signUpBbutton.Click += new System.EventHandler(this.signUpBbutton_Click);
+            // 
+            // WelcomeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 313);
+            this.Controls.Add(this.signUpBbutton);
+            this.Controls.Add(this.logInButton);
+            this.Controls.Add(this.pswrdTxtBox);
+            this.Controls.Add(this.usernameTxtBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "WelcomeForm";
+            this.Text = "e-Leaning";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usernameTxtBox;
+        private System.Windows.Forms.TextBox pswrdTxtBox;
+        private System.Windows.Forms.Button logInButton;
+        private System.Windows.Forms.Button signUpBbutton;
+    }
+}
+
---
+++ WelcomeForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class WelcomeForm : Form
+    {
+        public WelcomeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void signUpBbutton_Click(object sender, EventArgs e)
+        {
+            SignUp signUp = new SignUp();
+            signUp.Show();
+        }
+
+        private void logInButton_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = new MainMenu();
+            mainMenu.Show();
+
+        }
+    }
+}
